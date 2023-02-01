26e80b304483d434eaacd12b6a7d34f57b2b9044
Julian Mason <julian@Julians-MacBook-Air-2.local>
2023-02-01 02:51:45 -0800

    added end screen

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ var timer = document.querySelector("#timer");
 var questionEL = document.querySelector("#questions");
 var choicesList = document.querySelector("#choices");
 var feedback = document.querySelector("#feedback");
-var initials = document.querySelector("#initials");
+var initial = document.querySelector("#initial");
 var scores = document.querySelector("#highscores");
 var timeLeft = 60;
+var clockInt;
 
 var questions = [
     "What is the shortcut command for statup code in index.html?",
@@ -29,11 +30,11 @@ var correct = [
 function startTimer() {
     timer.textContent = "Time remaining: " + timeLeft;
 
-        timer = setInterval(function() {
-            timerCount--;
-            timer.textContent = timerCount;
-            if (timerCount <= 0) {
-                clearInterval(timer);
+        clockInt = setInterval(function() {
+            timeLeft--;
+            timer.textContent = "Time remaining: " + timeLeft
+            if (timeLeft <= 0) {
+                endQuiz();
             }
         }, 1000);    
     }
@@ -41,24 +42,47 @@ function startTimer() {
 function startQuiz (questionElIndex) {
     choicesList.replaceChildren();
     questionEL.textContent = questions[questionElIndex];
-    for (var i = 0; i<4; i += 1) {
+    for (var i = 0; i < 4; i += 1) {
         var list = document.createElement("li");
         list.textContent = choices[questionElIndex][i];
-        list.addEventListener("clcik", function () {
+        list.addEventListener("click", function () {
             if (event.target.textContent === correct[questionElIndex]) {
-                feedback.textContent = "Correct";
+                feedback.textContent = "Correct ";
             } else {
-                feedback.textContent = "Wrong, the correct answer is" + correct[questionIndex];
-                timeLeft = -5;
+                feedback.textContent = "Wrong, the correct answer is " + correct[questionElIndex];
+                timeLeft -= 5;
                 timer.textContent = "Time remaining " +timeLeft;
-            } if (questionElIndex === 5) {
+            } if (questionElIndex === 1) {
                 choicesList.replaceChildren();
                 questionEL.textContent = "";
-                endGame();
+                endQuiz();
+            } else {
+                startQuiz(questionElIndex + 1);
             }
-        })
+        });
         choicesList.appendChild(list);
     }
 }
 
-
+function endQuiz() {
+    initial.textContent = "Enter your initial ";
+    var initialBtn = document.createElement("input");
+    initialBtn.setAttribute("type", "text");
+    initial.appendChild(initialBtn);
+    var saveBtn = document.createElement("input");
+    saveBtn.setAttribute("type", "button");
+    saveBtn.setAttribute("value", "save");
+    initial.appendChild(saveBtn);
+    timer.style.display = "none";
+    saveBtn.addEventListener("click", function(){
+        var quizScore = document.createElement("li");
+        quizScore.textContent = "Initial and Score " + initialBtn.value + " " + timeLeft
+        scores.appendChild(quizScore)
+    });
+    clearInterval(clockInt);
+}
+startBtn.addEventListener("click", function(){
+    firstScreen.style.display = "none";
+    startTimer();
+    startQuiz(0);
+});
